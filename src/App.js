8a4afb335cfb4660f3a34f07a60c161b9d6a65e0
andiMartinezManjaro <agm006@gmail.com>
2022-06-28 23:51:54 -0300

    desafio card + buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './index.css';
 import NavBar from '../src/Components/NavBar';
 import ItemListContainer from '../src/Components/ItemListContainer';
 import Challenge from '../src/Components/Challenge';
+import ProductCard from './Components/ProductCard';
 
 function App() {
   return (
     <>
       
       <NavBar />
-      <ItemListContainer greeting="¡Bienvenidos a la Tienda!" />
+      <ItemListContainer greeting="¡Bienvenidos a la Tienda!">
+      
+      </ItemListContainer>
+      <ProductCard stock={10} min={0} name="DUMMY PRODUCT"/>
       <Challenge />
       
     </>
