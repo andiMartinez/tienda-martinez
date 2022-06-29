5af03ee7ab5a9511d48be5d6b2c699fdc8140b74
andiMartinezManjaro <agm006@gmail.com>
2022-06-28 23:56:18 -0300

    fix

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -24,8 +24,7 @@ const ProductCard = ({stock,min, name}) => {
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full border-solid">
         -
         </button>
-        <button 
-        onClick={()=>count(-1)} 
+        <button  
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full border-solid">
         <span>CANTIDAD: {amount}</span> - STOCK: {stock}
         </button>
